25f4e119f733fd3662779d2acca54d168ae9c685
abdullah <hemdana@seder.sa>
2022-04-24 00:31:31 +0200

    End ofsection11

---
+++ BackEnd/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Helpers;
+using API.Interfaces;
+using API.Services;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.OData;
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));  //strongly Types
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddControllers().AddOData(opt => opt.Select().OrderBy().Count().SetMaxTop(null).Filter());
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
